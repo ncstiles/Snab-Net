f8b6ffd0b30be80f8f30f39eedcb29083f080d8e
Nicole C Stiles <nstiles@satori-login-001.mit.edu>
2023-05-07 23:59:48 -0400

    viz 14

--- Models/networks/network.py
+++ Models/networks/network.py
@@ -84,7 +84,7 @@ def visualize_map(attn_map_3, attn_map_2, attn_map_1, ground_img):
 
         # get current time
         timestamp = datetime.now().strftime("%m_%d:%H_%M_%S.%f")
-        plt.savefig(f"attn_out/ablate_13_{timestamp}.png")
+        plt.savefig(f"attn_out/ablate_14_{timestamp}.png")
 
 
 class Comprehensive_Atten_Unet(nn.Module):
@@ -105,26 +105,27 @@ class Comprehensive_Atten_Unet(nn.Module):
 
         # downsampling
         self.conv2 = conv_block(self.in_channels, filters[0])
-        self.maxpool2 = nn.MaxPool2d(kernel_size=(8, 8))
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(4, 4))
 
-        self.conv4 = conv_block(filters[0], filters[3], drop_out=True)
-        self.maxpool4 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.conv3 = conv_block(filters[0], filters[2])
+        self.maxpool3 = nn.MaxPool2d(kernel_size=(4, 4))
 
-        self.center = conv_block(filters[3], filters[4], drop_out=True)
+        self.center = conv_block(filters[2], filters[4], drop_out=True)
 
         # attention blocks
-        self.attentionblock2 = MultiAttentionBlock(in_size=filters[0], gate_size=filters[3], inter_size=filters[1],
+        self.attentionblock2 = MultiAttentionBlock(in_size=filters[0], gate_size=filters[2], inter_size=filters[1],
                                                    nonlocal_mode=nonlocal_mode, sub_sample_factor=attention_dsample)
-        self.nonlocal4_2 = NONLocalBlock2D(in_channels=filters[4], inter_channels=filters[4] // 4)
+        self.attentionblock3 = MultiAttentionBlock(in_size=filters[2], gate_size=filters[4], inter_size=filters[2],
+                                                   nonlocal_mode=nonlocal_mode, sub_sample_factor=attention_dsample)
 
         # upsampling
-        self.up_concat4 = UpCat(filters[4], filters[3], self.is_deconv)
-        self.up_concat2 = UpCat(filters[3], filters[0], self.is_deconv)
-        self.up4 = SE_Conv_Block(filters[4], filters[3], drop_out=True)
+        self.up_concat3 = UpCat(filters[4], filters[2], self.is_deconv)
+        self.up_concat2 = UpCat(filters[2], filters[0], self.is_deconv)
+        self.up3 = SE_Conv_Block(filters[3], filters[2], drop_out=True)
         self.up2 = SE_Conv_Block(filters[1], filters[0])
 
         # deep supervision
-        self.dsv4 = UnetDsv3(in_size=filters[3], out_size=4, scale_factor=self.out_size)
+        self.dsv3 = UnetDsv3(in_size=filters[2], out_size=4, scale_factor=self.out_size)
         self.dsv2 = nn.Conv2d(in_channels=filters[0], out_channels=4, kernel_size=1)
 
         self.scale_att = scale_atten_convblock(in_size=8, out_size=4)
@@ -136,33 +137,37 @@ class Comprehensive_Atten_Unet(nn.Module):
         conv2 = self.conv2(inputs)
         maxpool2 = self.maxpool2(conv2)
 
-        conv4 = self.conv4(maxpool2)
-        maxpool4 = self.maxpool4(conv4)
+        conv3 = self.conv3(maxpool2)
+        maxpool3 = self.maxpool3(conv3)
 
         # Gating Signal Generation
-        center = self.center(maxpool4)
+        center = self.center(maxpool3)
 
         # Attention Mechanism
         # Upscaling Part (Decoder)
-        up4 = self.up_concat4(conv4, center)
-        g_conv4 = self.nonlocal4_2(up4)
-        up4, att_weight4 = self.up4(g_conv4)
+        g_conv3, att3 = self.attentionblock3(conv3, center)
         
-        upsample = nn.Upsample(scale_factor=4, mode='bilinear')
-        up4 = upsample(up4)
+        upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+        up_center = upsample(center)
+
+        up3 = self.up_concat3(g_conv3, up_center)
+        up3, att_weight3 = self.up3(up3)
         
-        g_conv2, att2 = self.attentionblock2(conv2, up4)
-        up2 = self.up_concat2(g_conv2, up4)
+        upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        up3 = upsample(up3)
+        
+        g_conv2, att2 = self.attentionblock2(conv2, up3)
+        up2 = self.up_concat2(g_conv2, up3)
         up2, att_weight2 = self.up2(up2)
 
         if viz: 
             print("visualizing")
-            visualize_map(None, att2, None, inputs)
+            visualize_map(att3, att2, None, inputs)
        
         # Deep Supervision
-        dsv4 = self.dsv4(up4)
+        dsv3 = self.dsv3(up3)
         dsv2 = self.dsv2(up2)
-        dsv_cat = torch.cat([dsv2, dsv4], dim=1)
+        dsv_cat = torch.cat([dsv2, dsv3], dim=1)
         out = self.scale_att(dsv_cat)
 
         out = self.final(out)
@@ -171,4 +176,3 @@ class Comprehensive_Atten_Unet(nn.Module):
 
         return out
         
-
